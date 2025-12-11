40c243d42d07cf8ddd092386f6853e3b446b95a2
AasathDevDS <aasatheng2003@gmail.com>
2025-12-11 14:04:47 +0530

    login and logout

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('blogs/<slug:slug>/', blogviews.get_page, name = 'get_page'),
     path('blog/search', blogviews.search, name = 'search'),
     path('register/', views.register, name = 'register'),
+    path('login/', views.login, name = 'login'),
+    path('logout', views.logout, name = 'logout')
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
